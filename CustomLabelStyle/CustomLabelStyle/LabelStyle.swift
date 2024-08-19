c31dc406b5d223f3e6d403f513b25547121d738b
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-08-19 17:49:08 +0200

    LABEL

---
+++ CustomLabelStyle/CustomLabelStyle/LabelStyle.swift
@@ -0,0 +1,11 @@
+//
+//  LabelStyle.swift
+//  CustomLabelStyle
+//
+//  Created by Manyuchi, Carrington C on 2024/08/19.
+//
+
+import UIKit
+
+
+
---
+++ CustomLabelStyle/CustomLabelStyle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomLabelStyle
+//
+//  Created by Manyuchi, Carrington C on 2024/08/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
